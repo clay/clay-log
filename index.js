1a8f70f92d350671b42d36944685f78d73de09be
Jon Winton <winton.jon@gmail.com>
2017-09-10 20:10:44 -0400

    Adding samples

---
+++ index.js
@@ -0,0 +1,60 @@
+'use strict'
+
+const pino = require('pino'),
+  pkg = require('./package.json');
+
+var logger;
+
+/**
+ * [init description]
+ * @param  {String} [name='' }]            [description]
+ * @return {[type]}          [description]
+ */
+function init({ name = '', pretty = false }) {
+  var pretty = pino.pretty({
+    levelFirst: true
+  })
+  pretty.pipe(process.stdout)
+
+  logger = pino({
+    name
+  }, pretty);
+
+  return log();
+}
+
+/**
+ * [meta description]
+ * @param  {[type]} options [description]
+ * @return {[type]}         [description]
+ */
+function meta(options) {
+  return log(logger.child(options));
+}
+
+/**
+ * Call the proper log level
+ * @param  {[type]} level     [description]
+ * @param  {[type]} msg       [description]
+ * @param  {Object} [data={}] [description]
+ * @return {[type]}           [description]
+ */
+function log(instanceLog) {
+  return function (level = 'info', msg = '', data = {}) {
+    if (level instanceof Error) {
+      msg = level;
+      level = 'error';
+    }
+
+    if (!level || !msg) {
+      instanceLog.error(new Error('level or msg arguments required'));
+    }
+
+    instanceLog.level = level; // Make sure level is set each time
+    instanceLog[level](data, msg);
+  }
+}
+
+module.exports.init = init;
+module.exports.meta = meta;
+module.exports.log = log;
---
+++ test.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * This could be an example service for logging. The user could write a service around
+ * the `init` and `meta` methods for consistent logging across the whole application
+ * and on a per-file basis.
+ *
+ * Furthermore, Pino has serializers that we can use to replicate the `SILLY` env var
+ * for showing EVERYTHING being logged ('verbose').
+ */
+const lib = require('./');
+
+lib.init({
+  name: 'Testing'
+});
+
+var log = lib.meta({
+  cool: 'cool!'
+}),
+anotherlog = lib.meta({
+  filename: __filename
+});
+
+log('info', 'womp womp', {work: 'plz'});
+console.log('\n\n');
+anotherlog('trace', 'wssfasdfasf', {asdf: 'dfdf'});
+console.log('\n\n');
+anotherlog('fatal', 'fatal error');
+console.log('\n\n');
+anotherlog('debug', 'debug log', { exampleElasticQuery: '{query: { term: { cool: "query"}}}'});
+console.log('\n\n');
+anotherlog(new Error('oh man this is an error'));
+anotherlog('error', new Error('specifying error level'));
+console.log('\n\n');
+anotherlog('warn', 'something isn\'t groovy', { something: 'notGroovy' });
